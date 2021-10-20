ffa05d3c70d647ad158543cbd89b638e9c501814
Emma Chio <emmany2409@gmail.com>
2021-10-20 00:43:02 -0400

    wrote humanOutputFile

--- Lab03-XOR/XOR.java
+++ Lab03-XOR/XOR.java
@@ -6,15 +6,15 @@ public class XOR{
   public static String numberOutput(String key, String message) throws FileNotFoundException {
     int kLength = key.length();
     int mLength = message.length();
-    char[] keyChars = new char[kLength];
-    char[] messageChars = new char[mLength];
+    int[] keyChars = new int[kLength];
+    int[] messageChars = new int[mLength];
     int[] xorList = new int[mLength];
     String result = "";
     for(int i = 0; i < kLength; i++) {
-      keyChars[i] = key.charAt(i);
+      keyChars[i] = (int)key.charAt(i);
     }
     for(int j = 0; j < mLength; j++) {
-      messageChars[j] = message.charAt(j);
+      messageChars[j] = (int)message.charAt(j);
     }
     for(int k = 0; k < mLength; k++) {
       int temp = k % kLength;
@@ -27,7 +27,7 @@ public class XOR{
   }
 
   public static String numberOutputFile(String keyFileName, String messageFileName) throws FileNotFoundException {
-    File plainText = new File (messageFileName);
+    File messageText = new File (messageFileName);
     File keyText = new File (keyFileName);
     String line;
     String key = "";
@@ -35,7 +35,7 @@ public class XOR{
     try{
       Scanner n = new Scanner(keyText);
       key = n.nextLine();
-      Scanner s = new Scanner(plainText);
+      Scanner s = new Scanner(messageText);
       while(s.hasNext()) {
         line = s.nextLine();
         result = result + numberOutput(key, line);
@@ -47,6 +47,57 @@ public class XOR{
     return result;
   }
 
+  public static int[] numberOutputInt(String key, String message) throws FileNotFoundException {
+    int kLength = key.length();
+    int mLength = message.length();
+    int[] keyChars = new int[kLength];
+    int[] messageChars = new int[mLength];
+    int[] xorList = new int[mLength];
+    for(int i = 0; i < kLength; i++) {
+      keyChars[i] = (int)key.charAt(i);
+    }
+    for(int j = 0; j < mLength; j++) {
+      messageChars[j] = (int)message.charAt(j);
+    }
+    for(int k = 0; k < mLength; k++) {
+      int temp = k % kLength;
+      xorList[k] = (messageChars[k] ^ keyChars[temp]);
+    }
+    return xorList;
+  }
+
+  // public static void humanOutput(String key, String message) {
+  //   try{
+  //   }
+  //   catch (IOException e) {
+  //     // e.printStrackTrace();
+  //     System.out.println("error");
+  //   }
+  // }
+
+  public static void humanOutputFile(String keyFileName, String messageFileName) {
+    File messageText = new File (messageFileName);
+    File keyText = new File (keyFileName);
+    String message;
+    String key = "";
+    try{
+      OutputStream output = new FileOutputStream("output");
+      Scanner n = new Scanner(keyText);
+      key = n.nextLine();
+      Scanner s = new Scanner(messageText);
+      while(s.hasNext()) {
+        message = s.nextLine();
+        int[] result = numberOutputInt(key, message);
+        for(int i = 0; i < result.length; i++) {
+          output.write(result[i]);
+        }
+      }
+    }
+    catch (IOException e) {
+      System.out.println("Sorry file not found");
+    }
+  }
+
   public static void main(String args[]) throws FileNotFoundException {
     String type = args[0];
     String keyFile = args[1];
@@ -54,7 +105,9 @@ public class XOR{
     if(type.equals("numOut")) {
       System.out.println(numberOutputFile(keyFile, messageFile));
     }
-    // System.out.println(numberOutput("A", "hello"));
+    else if(type.equals("human")) {
+      humanOutputFile(keyFile, messageFile);
+    }
   }
 
 }
